f013fc7b11af76003aa7edec46a3c5e0ecbcbeb4
SenthilRaja <mailtest151@gmail.com>
2026-01-13 16:14:11 +0100

    expand map to full width

--- mapdata.js
+++ mapdata.js
@@ -1,7 +1,7 @@
 var simplemaps_usmap_mapdata = {
   main_settings: {
     //General settings
-    width: "700", //or 'responsive'
+    width: "responsive", //or 'responsive'
     background_color: "#FFFFFF",
     background_transparent: "yes",
     popups: "detect",
